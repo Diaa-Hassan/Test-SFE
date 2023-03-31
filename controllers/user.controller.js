c342958d49a365c17bff4008d1c3a0c88a42be2e
A-bahaa <Ahmed180032@feng.bu.edu.eg>
2023-03-31 16:41:22 +0200

    deleteUser route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,7 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 
-const User = require('../models/user.model');
+const User = require("../models/user.model");
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,12 +9,12 @@ const signup = async (req, res, next) => {
   try {
     const usernamecheck = await User.findOne({ username: username });
     if (usernamecheck) {
-      return res.json({ msg: 'Username is already used.', status: false });
+      return res.json({ msg: "Username is already used.", status: false });
     }
 
     const emailcheck = await User.findOne({ email: email });
     if (emailcheck) {
-      return res.json({ msg: 'Email is already used.', status: false });
+      return res.json({ msg: "Email is already used.", status: false });
     }
 
     const id = crypto.randomBytes(2).toString("hex");
@@ -25,13 +25,31 @@ const signup = async (req, res, next) => {
     const obj = JSON.stringify(user);
     const jsonData = JSON.parse(obj);
 
-    return res.status(200).json('User Created Successfully');
-
+    return res.status(200).json("User Created Successfully");
   } catch (ex) {
     next(ex);
   }
 };
 
+const deleteUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json("Please provide id");
+    }
+
+    const user = await User.findOne({ id });
+    if (!user) {
+      return res.status(400).json("User not found");
+    }
+
+    await user.deleteOne();
+
+    return res.status(200).json("User Deleted Successfully");
+  } catch (ex) {
+    next(ex);
+  }
+};
 
 module.exports = {
   signup,
