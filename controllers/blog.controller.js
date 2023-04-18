91bb66bea4842fc2c96910c247a274d978a5b9bd
karim-moftah <karim180547@feng.bu.edu.eg>
2023-04-18 22:18:40 +0200

    getUserBlogs

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -15,26 +15,11 @@ const getAllBlogs = async (req, res, next) => {
   }
 
   return res.status(200).json({ blogs });
-};
+}
 
-const getById = async (req, res, next) => {
-  const id = req.params.id;
-  let blog;
-
-  try {
-    blog = await Blog.findById(id);
-  } catch (e) {
-    return console.log(e);
-  }
-
-  if (!blog) {
-    return res.status(500).json({ message: "not found" });
-  }
-
-  return res.status(200).json({ blog });
-};
 
 const addBlog = async (req, res, next) => {
+
   const { title, desc, img, user } = req.body;
 
   let existingUser;
@@ -47,10 +32,7 @@ const addBlog = async (req, res, next) => {
     return res.status(400).json({ message: " Unautorized" });
   }
   const blog = new Blog({
-    title,
-    desc,
-    img,
-    user,
+    title, desc, img, user
   });
 
   try {
@@ -60,22 +42,7 @@ const addBlog = async (req, res, next) => {
   }
 
   return res.status(200).json({ blog });
-};
-
-const deleteBlog = async (req, res, next) => {
-  const id = req.params.id;
-
-  let blog;
-  try {
-    blog = await Blog.findByIdAndRemove(id);
-  } catch (e) {
-    console.log(e);
-  }
-  if (!blog) {
-    return res.status(500).json({ message: "unable to delete" });
-  }
-  return res.status(200).json({ message: "successfuly deleted" });
-};
+}
 
 const updateBlog = async (req, res, next) => {
   const blogId = req.params.id;
@@ -86,25 +53,80 @@ const updateBlog = async (req, res, next) => {
     blog = await Blog.findById(blogId);
 
     if (!blog) {
-      return res.status(500).json({ message: "Unable to update" });
+      return res.status(500).json({ message: "Unable to update" })
     }
 
     blog.title = title;
     blog.desc = desc;
 
     await blog.save();
+
   } catch (e) {
     return console.log(e);
   }
 
   return res.status(200).json({ blog });
+}
+
+const getById = async (req, res, next) => {
+  const id = req.params.id;
+  let blog;
+
+  try {
+    blog = await Blog.findById(id);
+  }
+  catch (e) {
+    return console.log(e);
+  }
+
+  if (!blog) {
+    return res.status(500).json({ message: "not found" });
+  }
+
+  return res.status(200).json({ blog });
+}
+
+const deleteBlog = async (req, res, next) => {
+
+  const id = req.params.id;
+
+  let blog;
+  try {
+    blog = await Blog.findByIdAndRemove(id)
+  } catch (e) {
+    console.log(e);
+  }
+  if (!blog) {
+    return res.status(500).json({ message: "unable to delete" })
+  }
+  return res.status(200).json({ message: "successfuly deleted" })
+}
+
+const getByUserId = async (req, res, next) => {
+  const userId = req.params.id;
+  let userBlogs;
+  try {
+    const user = await User.findOne({ id: userId });
+
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found" });
+    }
+  } catch (err) {
+    return console.log(err);
+  }
+
+
+  try {
+    userBlogs = await Blog.find({ user: userId });
+  } catch (err) {
+    return console.log(err);
+  }
+
+  if (!userBlogs) {
+    return res.status(404).json({ message: "No Blog Found" });
+  }
+
+  return res.status(200).json({ user: userBlogs });
 };
 
-module.exports = {
-  getAllBlogs,
-  addBlog,
-  updateBlog,
-  getById,
-  deleteBlog,
-  getByUserId,
-};
+module.exports = { getAllBlogs, addBlog, updateBlog, getById, deleteBlog, getByUserId };
--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -1,19 +1,14 @@
-const express = require("express");
+const express = require("express")
 const blogRouter = express.Router();
 
-const {
-  getAllBlogs,
-  addBlog,
-  updateBlog,
-  getById,
-  deleteBlog,
-  getByUserId,
-} = require("../controllers/blog.controller");
+const { getAllBlogs, addBlog,
+  updateBlog, getById,
+  deleteBlog, getByUserId } = require("../controllers/blog.controller");
 
 blogRouter.get("/", getAllBlogs);
-blogRouter.get("/:id", getById);
-blogRouter.post("/add", addBlog);
+blogRouter.post('/add', addBlog);
 blogRouter.put("/update/:id", updateBlog);
+blogRouter.get("/:id", getById);
 blogRouter.delete("/:id", deleteBlog);
-
+blogRouter.get("/user/:id", getByUserId)
 module.exports = blogRouter;
